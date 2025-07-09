df1304c04e65000c19e9bf9c61c45c335fa5ba0c
KeranLuciel <121261528+KeranLuciel@users.noreply.github.com>
2025-07-09 09:45:27 -0500

    cambios de codigo index.php

--- index.php
+++ index.php
@@ -6,94 +6,91 @@
   <title>Datos Personales de Paciente</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" 
         integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <style>
+    .titulo {
+      color: red;
+    }
+    .container {
+      margin-top: 30px;
+    }
+  </style>
 </head>
 <body>
   <div class="container">
-    <div class="jumbotron mt-4">
-      <h1 class="display-4">
-        <?php 
-          if (isset($_GET['id_paciente'])) {
-            // Conexión a la base de datos
-            $conexion = mysqli_connect(getenv('35.238.38.29'), getenv('karen'), getenv('123456'), "practica");
-            if ($conexion) {
-              $id_paciente = $_GET['id_paciente'];
-              $cadenaSQL = "SELECT nombres FROM Pacientes WHERE id_paciente = $id_paciente";
-              $resultado = mysqli_query($conexion, $cadenaSQL);
-              if ($resultado && mysqli_num_rows($resultado) > 0) {
-                $fila = mysqli_fetch_assoc($resultado);
-                echo "Datos personales de " . $fila['nombres'];
-              }
-              mysqli_close($conexion);
-            } else {
-              echo "Error en la conexión";
-            }
+    <?php 
+      // Conexión a la base de datos
+      $conexion = mysqli_connect("35.238.38.29", "karen", "123456", "practica");
+      if (!$conexion) {
+        echo "<p>Hubo un error al conectar con la base de datos.</p>";
+      } else {
+        if (isset($_GET['id_paciente'])) {
+          // Mostrar los datos del paciente seleccionado
+          $id_paciente = $_GET['id_paciente'];
+          $cadenaSQL = "
+            SELECT 
+              p.nombres, p.apellidos, p.correo, p.fecha_nacimiento, p.telefonos,
+              e.descripcion AS enfermedad, e.gravedad
+            FROM 
+              Pacientes p
+            JOIN 
+              Enfermedades e ON p.id_enfermedades = e.id_enfermedades
+            WHERE 
+              p.id_paciente = $id_paciente
+          ";
+          $resultado = mysqli_query($conexion, $cadenaSQL);
+          
+          if ($resultado && mysqli_num_rows($resultado) > 0) {
+            $fila = mysqli_fetch_assoc($resultado);
+            echo "<h2 class='titulo'>Datos personales de " . $fila['nombres'] . "</h2>";
+            echo "<table class='table table-bordered mt-3'>
+                    <tr><th>Nombre</th><td>" . $fila['nombres'] . "</td></tr>
+                    <tr><th>Apellidos</th><td>" . $fila['apellidos'] . "</td></tr>
+                    <tr><th>Correo</th><td>" . $fila['correo'] . "</td></tr>
+                    <tr><th>Fecha de Nacimiento</th><td>" . $fila['fecha_nacimiento'] . "</td></tr>
+                    <tr><th>Teléfonos</th><td>" . $fila['telefonos'] . "</td></tr>
+                    <tr><th>Enfermedad</th><td>" . $fila['enfermedad'] . "</td></tr>
+                    <tr><th>Gravedad</th><td>" . $fila['gravedad'] . "</td></tr>
+                  </table>";
           } else {
-            echo "Seleccione un paciente";
+            echo "<p>No se encontraron datos para este paciente.</p>";
           }
-        ?>
-      </h1>
-      <p class="lead">Aplicación para visualizar los detalles de los pacientes</p>
-      <hr class="my-4">
-      <p>Conexión PHP con MySQL</p>
-    </div>
-
-    <?php
-    if (isset($_GET['id_paciente'])) {
-      // Mostrar los datos del paciente seleccionado
-      $conexion = mysqli_connect(getenv('35.238.38.29'), getenv('karen'), getenv('123456'), "practica");
-
-      if ($conexion) {
-        $id_paciente = $_GET['id_paciente'];
-        $cadenaSQL = "
-          SELECT 
-            p.nombres, p.apellidos, p.correo, p.fecha_nacimiento, p.telefonos,
-            e.descripcion AS enfermedad, e.gravedad
-          FROM 
-            Pacientes p
-          JOIN 
-            Enfermedades e ON p.id_enfermedades = e.id_enfermedades
-          WHERE 
-            p.id_paciente = $id_paciente
-        ";
-
-        $resultado = mysqli_query($conexion, $cadenaSQL);
-        if ($resultado && mysqli_num_rows($resultado) > 0) {
-          $fila = mysqli_fetch_assoc($resultado);
-          echo "<table class='table table-bordered'>
-                  <tr><th>Nombre</th><td>" . $fila['nombres'] . "</td></tr>
-                  <tr><th>Apellidos</th><td>" . $fila['apellidos'] . "</td></tr>
-                  <tr><th>Correo</th><td>" . $fila['correo'] . "</td></tr>
-                  <tr><th>Fecha de Nacimiento</th><td>" . $fila['fecha_nacimiento'] . "</td></tr>
-                  <tr><th>Teléfonos</th><td>" . $fila['telefonos'] . "</td></tr>
-                  <tr><th>Enfermedad</th><td>" . $fila['enfermedad'] . "</td></tr>
-                  <tr><th>Gravedad</th><td>" . $fila['gravedad'] . "</td></tr>
-                </table>";
         } else {
-          echo "<p>No se encontraron datos para este paciente.</p>";
+          // Mostrar lista de pacientes con enlaces para ver detalles
+          $cadenaSQL = "SELECT id_paciente, nombres FROM Pacientes";
+          $resultado = mysqli_query($conexion, $cadenaSQL);
+          echo "<h3>Lista de Pacientes</h3><ul class='list-group'>";
+          while ($fila = mysqli_fetch_assoc($resultado)) {
+            echo "<li class='list-group-item'><a href='?id_paciente=" . $fila['id_paciente'] . "'>" . $fila['nombres'] . "</a></li>";
+          }
+          echo "</ul>";
         }
-        mysqli_close($conexion);
-      } else {
-        echo "<p>Hubo un error al conectar con la base de datos.</p>";
-      }
-    } else {
-      // Mostrar lista de pacientes con enlaces para ver detalles
-      $conexion = mysqli_connect(getenv('35.238.38.29'), getenv('karen'), getenv('123456'), "practica");
 
-      if ($conexion) {
-        $cadenaSQL = "SELECT id_paciente, nombres FROM Pacientes";
+        // Mostrar los pacientes cuyo nombre no empieza con una vocal
+        $cadenaSQL = "
+          SELECT id_paciente, nombres
+          FROM Pacientes
+          WHERE LEFT(nombres, 1) NOT IN ('A', 'E', 'I', 'O', 'U')
+        ";
         $resultado = mysqli_query($conexion, $cadenaSQL);
-        echo "<h3>Lista de Pacientes</h3><ul class='list-group'>";
-        while ($fila = mysqli_fetch_assoc($resultado)) {
-          echo "<li class='list-group-item'><a href='?id_paciente=" . $fila['id_paciente'] . "'>" . $fila['nombres'] . "</a></li>";
+        if (mysqli_num_rows($resultado) > 0) {
+          echo "<h3 class='mt-4'>Pacientes cuyo nombre no empieza con una vocal</h3>";
+          echo "<table class='table table-bordered'>
+                  <thead>
+                    <tr><th>ID Paciente</th><th>Nombre</th></tr>
+                  </thead>
+                  <tbody>";
+          while ($fila = mysqli_fetch_assoc($resultado)) {
+            echo "<tr><td>" . $fila['id_paciente'] . "</td><td>" . $fila['nombres'] . "</td></tr>";
+          }
+          echo "</tbody></table>";
+        } else {
+          echo "<p>No hay pacientes cuyo nombre no empiece con una vocal.</p>";
         }
-        echo "</ul>";
-        mysqli_close($conexion);
-      } else {
-        echo "<p>Hubo un error al conectar con la base de datos.</p>";
-      }
-    }
-    ?>
 
+        // Cerrar conexión
+        mysqli_close($conexion);
+      }
+    ?>
   </div>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
@@ -107,5 +104,4 @@
           crossorigin="anonymous"></script>
 </body>
 </html>
-
 
